a3a8dbf7fd0a1fa95f8150063f3ff4d02bf100ac
23456-Tony-Larrea <52183482+23456-Tony-Larrea@users.noreply.github.com>
2021-08-23 18:50:28 -0500

    options crud

--- src/conexionmysql1/ConexionMysql1.java
+++ src/conexionmysql1/ConexionMysql1.java
@@ -7,6 +7,8 @@ package conexionmysql1;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -27,32 +29,124 @@ public class ConexionMysql1 {
         String clave;
         int estado;
         String correo;
+        int codigo;
         
-       usuario="Tony";
-       clave="12399912";
-       estado=5;
-       correo="avs@yavirac";        
-        resultadodeConsulta=0;
-        try {
-            //conexion a la base de datos Mysql
+         String op="";//se crea una variable que recibira la opcion 
+ 
+        do{
+            op=JOptionPane.showInputDialog("a. Insertar\n b. Actualizar\n c. Eliminar\n d. Observar\n e. Salir\n"); 
+            if(op.equals("a")){//si se elige la opcion a se muestra el siguiente mensaje
+                JOptionPane.showMessageDialog(null, "esto es insertar.");
+                usuario=JOptionPane.showInputDialog("null","ingresa el usuario");
+                clave=JOptionPane.showInputDialog("null","ingresa la clave");     
+                estado = Integer.parseInt(JOptionPane.showInputDialog("null", "ingresa el estado"));
+                correo=JOptionPane.showInputDialog("null","ingresa el correo");
+          try {
+           //conexion a la base de datos Mysql
             con.conectarMySQL();
             UsuarioDao usDao= new UsuarioDao();
             resultadodeConsulta=usDao.Insertar(usuario, clave, estado, correo);
             if (resultadodeConsulta == 1){
-                System.out.println("Usuario Insertado correctamente....");
-                usDao.Actualizar(4, usuario, clave, estado, correo);
-                usDao.Eliminar(1);
-                usDao.ConsultarUsuario();
+                System.out.println("Usuario Insertado correctamente....\n");
                
-
             }else
             {
-                System.err.println("No se pudo insertar el usuario...");
+                System.err.println("No se pudo insertar el usuario...\n");
                 return;
             }
             }catch (Exception e){
             System.err.println("Error de consulta " +e.getMessage());
             
         }
+            }
+            else if(op.equals("b")){//si se elige la opcion b se muestra el siguiente mensaje
+                JOptionPane.showMessageDialog(null, "esto es actualizar.");
+                codigo = Integer.parseInt(JOptionPane.showInputDialog("null", "ingresa el ID para actualizar"));
+                 usuario=JOptionPane.showInputDialog("null","ingresa el usuario a actualizar");
+                clave=JOptionPane.showInputDialog("null","ingresa la clave a acualizar");     
+                estado = Integer.parseInt(JOptionPane.showInputDialog("null", "ingresa el estado a acualizar"));
+                correo=JOptionPane.showInputDialog("null","ingresa el correo a acualizar");
+                      try {
+           //conexion a la base de datos Mysql
+            con.conectarMySQL();
+            UsuarioDao usDao= new UsuarioDao();
+            resultadodeConsulta=usDao.Actualizar(codigo, usuario, clave, estado, correo);
+            if (resultadodeConsulta == 1){
+                System.out.println("Usuario actualizado correctamente....\n");
+               
+            }else
+            {
+                System.err.println("No se pudo actualizar el usuario...\n");
+                return;
+            }
+            }catch (Exception e){
+            System.err.println("Error de consulta " +e.getMessage());
+            
+        }  
+                
+            }
+            else if(op.equals("c")){//si se elige la opcion c se muestra el siguiente mensaje
+                JOptionPane.showMessageDialog(null, "Esto es Eliminar");
+                        
+                codigo = Integer.parseInt(JOptionPane.showInputDialog("null", "ingresa el ID para actualizar"));
+                      try {
+           //conexion a la base de datos Mysql
+            con.conectarMySQL();
+            UsuarioDao usDao= new UsuarioDao();
+            resultadodeConsulta=usDao.Eliminar(codigo);
+            if (resultadodeConsulta == 1){
+                System.out.println("Usuario eliminado correctamente....\n");
+               
+            }else
+            {
+                System.err.println("No se pudo eliminado el usuario...\n");
+                return;
+            }
+            }catch (Exception e){
+            System.err.println("Error de consulta " +e.getMessage());
+            
+        }
+            }
+             else if(op.equals("d")){//si se elige la opcion d se cierra o finaliza la aplicacion
+                try{
+           //conexion a la base de datos Mysql
+            con.conectarMySQL();
+            UsuarioDao usDao= new UsuarioDao();
+            usDao.ConsultarUsuario();
+                
+                }catch(Exception e){
+                            System.err.println("Error de consulta " +e.getMessage());
+
+                }
+            }
+           
+            else if(op.equals("e")){//si se elige la opcion d se cierra o finaliza la aplicacion
+                System.exit(0);//comando para cerrar las aplicaciones
+            }
+            else{//si la opcion ingresada es diferente a las mostradas se muestra el siguiente mensaje
+                JOptionPane.showMessageDialog(null, "Ingrese una opcion valida");
+            }
+        }while(!op.equals("e"));//finaliza el ciclo con la condicional que hay dentro del whil
+          
+//       try {
+//           //conexion a la base de datos Mysql
+//            con.conectarMySQL();
+//            UsuarioDao usDao= new UsuarioDao();
+//            resultadodeConsulta=usDao.Insertar(usuario, clave, estado, correo);
+//            if (resultadodeConsulta == 1){
+//                System.out.println("Usuario Insertado correctamente....\n");
+//               
+//            }else
+//            {
+//                System.err.println("No se pudo insertar el usuario...\n");
+//                return;
+//            }
+//            }catch (Exception e){
+//            System.err.println("Error de consulta " +e.getMessage());
+//            
+//        }
+       
+        
     }
+
 }
